79e8ab1f527ac6d1bfeb5f56ef0329740e878d5e
Michael Ferguson <fergs@unboundedrobotics.com>
2013-11-25 14:16:07 -0800

    master branch is history

--- ros_arduino_firmware/src/libraries/ROSArduinoBridge/encoder_driver.h
+++
@@ -1,8 +0,0 @@
-/* *************************************************************
-   Encoder driver function definitions - by James Nugen
-   ************************************************************ */
-   
-long readEncoder(int i);
-void resetEncoder(int i);
-void resetEncoders();
-
--- ros_arduino_firmware/src/libraries/ROSArduinoBridge/motor_driver.h
+++
@@ -1,7 +0,0 @@
-/***************************************************************
-   Motor driver function definitions - by James Nugen
-   *************************************************************/
-
-void initMotorController();
-void setMotorSpeed(int i, int spd);
-void setMotorSpeeds(int leftSpeed, int rightSpeed);
