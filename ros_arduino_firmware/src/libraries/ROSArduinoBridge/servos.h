ddecaa0b2cd33de0470e006167f6cbab40f65ea1
theroboticsheep <nate@updroid.com>
2015-04-17 14:52:46 -0700

    added speed controlled servo sweep

--- ros_arduino_firmware/src/libraries/ROSArduinoBridge/servos.h
+++ ros_arduino_firmware/src/libraries/ROSArduinoBridge/servos.h
@@ -1,9 +1,35 @@
-/* Define the attachment of any servos here.
-   The example shows two servos attached on pins 3 and 5.
-*/
+#ifndef SERVOS_H
+#define SERVOS_H
 
-#define N_SERVOS 2
 
-Servo servos [N_SERVOS];
-byte servoPins [N_SERVOS] = {3, 5};
+#define N_SERVOS 1
 
+// This delay in milliseconds determines determines the pause 
+// between each one degree step the servo travels.  Increasing 
+// this number will make the servo sweep more slowly.  
+// Decreasing this number will make the servo sweep more quickly.  
+#define SWEEP_COMMAND_INTERVAL 150 // ms
+
+byte servoPins [N_SERVOS] = { 3 };
+byte servoInitPosition [N_SERVOS] = { 90 }; // degrees
+
+
+class SweepServo
+{
+  public:
+    SweepServo();
+    void initServo(int servoPin, int initPosition);
+    void doSweep();
+    void setTargetPosition(int position);
+    Servo getServo();
+
+  private:
+    Servo servo;
+    int currentPositionDegrees;
+    int targetPositionDegrees;
+    long lastSweepCommand;
+};
+
+SweepServo servos [N_SERVOS];
+
+#endif
